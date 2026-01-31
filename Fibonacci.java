ce8ddf35ea3f9cad3c1a882807a8aac022f201ba
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-01-31 16:48:32 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class  Fibonacci{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter number of terms");
+int n=sc.nextInt();
+int a=1,b=1;
+for(int i=1;i<=n;i++)
+{
+System.out.println(a +" ");
+int c=a+b;
+a=b;
+b=c;
+}
+}
+}
