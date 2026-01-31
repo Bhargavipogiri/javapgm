844c6281dd8bac95d296c51fff1729735584b781
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-01-31 16:47:13 +0530

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,22 @@
+class DataTypeDemo{
+public static void main(String args[])
+{
+int i=20;
+byte b=10;
+short s=30;
+long l=123456;
+float f=0.2f;
+double d=0.4;
+char c='A';
+boolean flag=true;
+System.out.println("byte value:"+b);
+System.out.println("int value:"+i);
+System.out.println("short value:"+s);
+System.out.println("long value:"+l);
+System.out.println("float value:"+f);
+System.out.println("double value:"+d);
+System.out.println("char value:"+c);
+System.out.println("boolean value:"+flag);
+
+}
+}
