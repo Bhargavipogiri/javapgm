0ad32c165d1f9592e9e6c8f9e4f96f9653808179
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-02-18 16:45:31 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class TypeConversion{
+public static void main(String args[])
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a and b value");
+ a=sc.nextInt();
+ b=sc.nextInt();
+System.out.println("addition:"+(a+b));
+System.out.println("diffrence:"+(a-b));
+System.out.println("product:"+(a*b));
+System.out.println("division:"+(a/b));
+}
+}
+
+
+
+
