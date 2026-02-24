f1db46e41ae3f8d8f8a0b8ad9fba4bf08c67df6f
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-02-24 17:43:02 +0530

    Add files via upload

---
+++ AssignmentExpressionExample.java
@@ -0,0 +1,7 @@
+public class AssignmentExpressionExample { 
+    public static void main(String[] args) { 
+        int x = 10; 
+        x += 5; // Same as x = x + 5; 
+        System.out.println(x); // 15 
+    } 
+}
