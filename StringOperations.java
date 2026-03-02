a5aee8fc4f35395b8063e4c477b26464948aaecc
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-03-02 16:58:12 +0530

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Length: " + str.length());
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+        System.out.println("Reversed: " + new StringBuilder(str).reverse());
+    }
+}
