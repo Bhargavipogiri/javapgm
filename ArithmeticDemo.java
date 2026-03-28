5962017eaef71cb8ea9754bc85bf52b4a25b3bf4
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-03-28 21:47:28 +0530

    Add files via upload

---
+++ ArithmeticDemo.java
@@ -0,0 +1,12 @@
+class ArithmeticDemo {
+    public static void main(String args[]) {
+        int a = 10, b = 0;
+
+        try {
+            int c = a / b;
+            System.out.println(c);
+        } catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
