9c529b29db97e362f6da4465e27309b7eace6bbc
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-03-29 18:24:40 +0530

    Add files via upload

---
+++ ThrowsDemo.java
@@ -0,0 +1,13 @@
+class ThrowsDemo {
+    static void check() throws ArithmeticException {
+        int a = 10 / 0;
+    }
+
+    public static void main(String args[]) {
+        try {
+            check();
+        } catch(ArithmeticException e) {
+            System.out.println("Handled in main");
+        }
+    }
+}
