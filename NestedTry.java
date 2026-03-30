a2a9a2b0450a96f0ee357886b0db41306b358181
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-03-30 20:40:11 +0530

    Add files via upload

---
+++ NestedTry.java
@@ -0,0 +1,17 @@
+class NestedTry {
+    public static void main(String args[]) {
+        try {
+            try {
+                int a = 10 / 0;
+            } catch(ArithmeticException e) {
+                System.out.println("Inner catch");
+            }
+
+            int arr[] = {1,2,3};
+            System.out.println(arr[5]);
+
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Outer catch");
+        }
+    }
+}
