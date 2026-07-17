f59b84c246430983087ae6baeac96bc1a1a63f0d
Bhargavipogiri <bhargavipogiri2006@gmail.com>
2026-07-17 20:48:36 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 5, 1, 6, 3};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println("Array after removing duplicates:");
+        for (int num : set) {
+            System.out.print(num + " ");
+        }
+    }
+}
