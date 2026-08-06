577bd2544cb0a5ea4491342c31f73d97ed4a3ce4
Bhargavi Pogiri <bhargavipogiri2006@gmail.com>
2026-08-06 21:12:31 +0530

    Add files via upload

---
+++ BestTimeToBuyStock.java
@@ -0,0 +1,19 @@
+public class BestTimeToBuyStock {
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int min = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+            else if (prices[i] - min > profit)
+                profit = prices[i] - min;
+        }
+
+        System.out.println("Maximum Profit = " + profit);
+    }
+}
