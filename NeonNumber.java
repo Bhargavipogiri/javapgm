61195fe18561030880e1dc8725731adcdc50755a
Bhargavi Pogiri <bhargavipogiri2006@gmail.com>
2026-08-14 22:02:41 +0530

    Add files via upload

---
+++ NeonNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NeonNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int square = n * n;
+        int sum = 0;
+
+        while (square > 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+
+        System.out.println(sum == n ? "Neon Number" : "Not a Neon Number");
+
+        sc.close();
+    }
+}
