22fcb62eeb093e6dac4862b41670243d459bdb6c
Bhargavi Pogiri <bhargavipogiri2006@gmail.com>
2026-08-31 19:40:52 +0530

    Add files via upload

---
+++ PerfectSquare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int root = (int) Math.sqrt(n);
+
+        if (root * root == n)
+            System.out.println("Perfect Square");
+        else
+            System.out.println("Not a Perfect Square");
+
+        sc.close();
+    }
+}
